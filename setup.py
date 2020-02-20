e345d3ea2f43db38cc71acd6e4cb22b157b5d2fb
josegit88 <jose.astroph@gmail.com>
2020-02-20 19:06:47 -0300

    prueba tox v1

---
+++ setup.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of the
+#   SCORPIO Project (https://github.com/josegit88/SCORPIO).
+# Copyright (c) 2020, Jose Benavides
+# License: MIT
+#   Full Text: https://github.com/josegit88/SCORPIO/blob/master/LICENSE
+
+
+# =============================================================================
+# DOCS
+# =============================================================================
+
+"""This file is for distribute and install SCORPIO
+"""
+
+
+# =============================================================================
+# IMPORTS
+# =============================================================================
+
+import os
+import pathlib
+
+from ez_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup
+
+
+# =============================================================================
+# CONSTANTS
+# =============================================================================
+
+# REQUIREMENTS = ["numpy", "scipy", "attrs", "matplotlib"]
+REQUIREMENTS = ["numpy", "astropy", "astroquery"]
+
+PATH = pathlib.Path(os.path.abspath(os.path.dirname(__file__)))
+
+with open(PATH / "README.md") as fp:
+    LONG_DESCRIPTION = fp.read()
+
+with open(PATH / "scorpio" / "__init__.py") as fp: # con mayuscula o minusc?
+# with open(PATH / "__init__.py") as fp: # con mayuscula o minusc?
+    for l in fp.readlines():
+        if l.startswith("__version__ = "):
+            VERSION = l.split("=", 1)[-1].replace('"', '').strip()
+            break
+
+
+# DESCRIPTION = "Grid Search in Python"
+DESCRIPTION = """SCORPIO: Sky COllector of galaxy Pairs and Image Output.
+    SCORPIO is a tool for download fits of astronomial survey and generate
+    image.
+    """
+
+
+# =============================================================================
+# FUNCTIONS
+# =============================================================================
+
+def do_setup():
+    setup(
+        name="scorpio",
+        version=VERSION,
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        long_description_content_type='text/markdown',
+
+        author=[
+            "Jose Benavides"],
+        author_email="jose.astroph@gmail.com",
+        url="https://github.com/josegit88/SCORPIO",
+        license="MIT",
+
+        keywords=["scorpio", "survey", "images", "stacking"],
+
+        classifiers=[
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: Education",
+            "Intended Audience :: Science/Research",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: Implementation :: CPython",
+            "Topic :: Scientific/Engineering"],
+
+        packages=["scorpio"],
+        py_modules=["ez_setup"],
+
+        install_requires=REQUIREMENTS)
+
+
+if __name__ == "__main__":
+    do_setup()
